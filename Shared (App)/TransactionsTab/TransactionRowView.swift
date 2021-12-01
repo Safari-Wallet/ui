fc4b8d9a9338926883f33c565b36d0fad40707e9
bejitono <stdemicheli@protonmail.com>
2021-12-01 18:58:39 +0700

    Fixes tx row issue.

--- Shared (App)/TransactionsTab/TransactionRowView.swift
+++ Shared (App)/TransactionsTab/TransactionRowView.swift
@@ -35,9 +35,8 @@ struct TransactionRowView: View {
                 }
                 .lineLimit(1)
             }
-            .frame(maxWidth: 180)
             Spacer()
-            VStack(spacing: 5) {
+            VStack(alignment: .trailing, spacing: 5) {
                 Group {
                     Text(amount) + Text(" ") + Text("ETH")
                 }
