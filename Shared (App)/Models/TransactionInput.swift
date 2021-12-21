7b182e13b5a53bb91901b4675ba4ea977363d62b
bejitono <stdemicheli@protonmail.com>
2021-12-21 16:27:51 +0700

    Defines tx input model.

---
+++ Shared (App)/Models/TransactionInput.swift
@@ -0,0 +1,12 @@
+//
+//  TransactionInput.swift
+//  Wallet
+//
+//  Created by Stefano on 17.12.21.
+//
+
+struct TransactionInput {
+    let methodName: String
+    let methodHash: String
+    let inputs: [String: Any]
+}
