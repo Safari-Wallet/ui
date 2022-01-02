4d3da493d8e80ed84c0a1af7dbceae1ffc2da6c1
bejitono <stdemicheli@protonmail.com>
2022-01-02 09:49:57 +0700

    Updates tx list view.

--- Shared (App)/TransactionsTab/TransactionsView.swift
+++ Shared (App)/TransactionsTab/TransactionsView.swift
@@ -14,89 +14,36 @@ struct TransactionsView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Section {
-                    Picker("Mode", selection: $viewModel.filter, content: {
-                        Text("All").tag(TransactionFilter.all)
-                        Text("Sent").tag(TransactionFilter.sent)
-                        Text("Received").tag(TransactionFilter.received)
-                        Text("Interactions").tag(TransactionFilter.interactions)
-                        Text("Failed").tag(TransactionFilter.failed)
-                    })
-                        .pickerStyle(SegmentedPickerStyle())
-                    List {
-                        switch viewModel.state {
-                        case .loading:
-                            ForEach(1..<6) { transactionGroup in
-                                //                            TransactionRow(tx: .placeholder)
-                                //                                .redacted(reason: .placeholder)
+                List {
+                    ForEach(viewModel.viewModels) { tx in
+                        TransactionRowView(
+                            txType: tx.type,
+                            description: "none",
+                            toAddress: tx.toAddress ?? "", // TODO: distinguish between contract address and send from or to
+                            token: tx.token,
+                            fiat: tx.fiat
+                        )
+                            .padding([.top, .bottom], 8)
+                            .onAppear {
+//                                viewModel.fetchTransactionsIfNeeded(currentTransaction: transactionGroup)
                             }
-                        case .fetched(txs: let txs):
-                            ForEach(txs) { transactionGroup in
-                                NavigationLink(destination: TransactionDetailsView(group: transactionGroup)) {
-                                    TransactionRowView(
-                                        txType: TransactionType(transactionGroup.type),
-                                        description: transactionGroup.description,
-//                                        toAddress: transactionGroup.toAddress,
-                                        toAddress: transactionGroup.contractName ?? "",
-                                        amount: transactionGroup.value
-                                    )
-                                    .onAppear {
-                                        viewModel.fetchTransactionsIfNeeded(currentTransaction: transactionGroup)
-                                    }
-                                }
-                            }
-                        case .error(message: let message):
-                            // Simple error msg for now
-                            Text(message)
-                            Spacer()
-                        }
                     }
-                    .refreshable {
-                        viewModel.fetchTransactions()
-                    }
-                    .listStyle(.plain)
                 }
-            }.navigationBarHidden(true)
-        }
-    }
-}
-
-struct TransactionRow: View {
-    
-    let transactionGroup: TransactionGroup
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            Text("Hash: \(transactionGroup.transactionHash)")
-                .font(.headline)
-                .bold()
-                .lineLimit(1)
-                .truncationMode(.tail)
-            HStack {
-                Text(transactionGroup.fromAddress)
-                    .lineLimit(1)
-                    .truncationMode(.middle)
-                Spacer()
-                Image(systemName: "arrow.right")
-                    .foregroundColor(.blue)
-                    .unredacted()
-                Spacer()
-                Text(transactionGroup.toAddress)
-                    .lineLimit(1)
-                    .truncationMode(.middle)
+                .refreshable {
+                    viewModel.fetchTransactions()
+                }
+                .listStyle(.plain)
+                .navigationTitle("Transactions")
             }
-            Text("\(transactionGroup.transactions.count) sources")
         }
-        .frame(maxWidth: .infinity)
-        .padding()
     }
-}
-
-struct TransactionsView_Previews: PreviewProvider {
-    static var previews: some View {
-        TransactionsView(viewModel: TransactionsListViewModel(chain: "1",
-                                                              address: "ric.eth",
-                                                              currency: "USD",
-                                                              symbol: "$"))
+    
+    struct TransactionsView_Previews: PreviewProvider {
+        static var previews: some View {
+            TransactionsView(viewModel: TransactionsListViewModel(chain: "1",
+                                                                  address: "ric.eth",
+                                                                  currency: "USD",
+                                                                  symbol: "$"))
+        }
     }
 }
