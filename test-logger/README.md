2fdf462d13899470e1da27d48d08f5dda64416f4
natclark <39421782+natclark@users.noreply.github.com>
2022-01-05 12:55:55 -0800

    add basic web view for console; connect to TS logger

--- test-logger/README.md
+++ test-logger/README.md
@@ -6,14 +6,12 @@
 npm i
 ```
 
-## Configure
-
-- Edit the `.env` file to configure the port number.
-
-- In the `.env` file, you can also toggle `VERBOSE` - which if `true`, logs each attempt the app makes to add a log (very meta!)
-
 ## Run
 
 ```sh
 node server.js
 ```
+
+Then check it out at http://localhost:8081
+
+*For a JSON of all the logs, see http://localhost:8081/json*
--- test-logger/server.js
+++ test-logger/server.js
@@ -1,12 +1,5 @@
-const dotenv = require(`dotenv`);
-const process = require(`process`);
 const express = require(`express`);
 const cors = require(`cors`);
-
-dotenv.config();
-
-const PORT = process.env.PORT;
-const VERBOSE = process.env.VERBOSE;
 
 let responseData = {
     logs: [],
@@ -21,18 +14,23 @@ app.use(cors({
     origin: `*`,
 }));
 
+app.use('/', express.static(`public`));
+
 app.get(`*`, (req, res) => {
-    res.set(`Content-Type`, `application/json`);
-    switch (req.url) {
-        case `/`:
-            res.writeHead(200);
-            res.end(JSON.stringify(responseData));
-            break;
-        default:
-            res.writeHead(404);
-            res.end(JSON.stringify({
-                error: `Not found.`,
-            }));
+    if (req.url !== `/`) {
+        switch (req.url) {
+            case `/json`:
+                res.set(`Content-Type`, `application/json`);
+                res.writeHead(200);
+                res.end(JSON.stringify(responseData));
+                break;
+            default:
+                res.set(`Content-Type`, `application/json`);
+                res.writeHead(404);
+                res.end(JSON.stringify({
+                    error: `Not found.`,
+                }));
+        }
     }
 });
 
@@ -52,17 +50,25 @@ app.post(`/add`, (req, res) => {
             error: false,
             message: `Success.`,
         }));
-        !!VERBOSE && (console.log(`New log from ${senderContext}: "${message}" @ ${timestamp}`));
     } else {
         res.writeHead(500);
         res.end(JSON.stringify({
             error: true,
             message: `Unable to add log.`,
         }));
-        !!VERBOSE && (console.log(`Failed to add a log.`));
     }
 });
 
-const startup = () => console.log(`Listening for requests at http://localhost:${PORT}`);
+app.post(`/clear`, (req, res) => {
+    res.set(`Content-Type`, `application/json`);
+    responseData.logs = [];
+    res.writeHead(200);
+    res.end(JSON.stringify({
+        error: false,
+        message: `Success.`,
+    }));
+});
 
-app.listen(PORT, startup);
+const startup = () => console.log(`Listening for requests at http://localhost:8081`);
+
+app.listen(8081, startup);
