1abc267012ee288e65c6b6a8bb8f8e09cd45e160
Ronald Mannak <ronaldmannak@me.com>
2022-01-12 21:23:19 -0800

    Copy address from sendView

--- Shared (App)/SendView/SendView.swift
+++ Shared (App)/SendView/SendView.swift
@@ -6,10 +6,38 @@
 //
 
 import SwiftUI
+//import UniformTypeIdentifiers
 
 struct SendView: View {
+    
+    @EnvironmentObject var userSettings: UserSettings
+    @State var to: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+         
+            // MARK: - From
+            Section(header: Text("From")) {
+                Text(userSettings.address?.addressString ?? "Error: select address in Settings")
+            }
+            .onTapGesture {
+                guard let address = userSettings.address?.addressString else { return }
+                #if os(iOS)
+//                UIPasteboard.general.setValue(address, forPasteboardType:  UTTypePlainText)
+                UIPasteboard.general.string = address
+                #elseif os(OSX)
+                let pasteBoard = NSPasteboard.general()
+                pasteBoard.clearContents()
+                pasteBoard.writeObjects([address as NSString])
+                #endif
+            }
+            
+            // MARK: - To
+            Section(header: Text("TO")) {
+                TextField("To address or ENS name", text: $to)                
+            }
+         
+        }
     }
 }
 
